dae99447f09c269f9b5af15027a6f82449790b24
Ilya <hacharoky86@gmail.com>
2020-06-26 23:25:20 +0300

    1 day

---
+++ ExtremeCode/TernarnyOperator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+    public TernarnyOperator()
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            string result = x > 100 ? "боольше 100" : "меньше ста";
+            Console.WriteLine(Convert.ToString(Number) + result);
+            Console.ReadLine();
+
+        }
+    }
+}
